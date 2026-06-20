373dcdb50a0782b7e047205750586e280ef5f711
Abhishek <abhishekyalla0709@gmail.com>
2026-06-20 12:26:48 +0530

    Commit - main branch

---
+++ bulls-cows-backend/src/main/java/com/abhishek/bullscows/BullsCowsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.abhishek.bullscows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullsCowsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullsCowsBackendApplication.class, args);
+	}
+
+}
---
+++ bulls-cows-backend/src/test/java/com/abhishek/bullscows/BullsCowsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.bullscows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullsCowsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
